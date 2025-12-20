73fcb6cdc0bf17ddb40b7c030c21d65b72cea3c7
Vedant Pareek <dunefro@gmail.com>
2025-12-20 22:51:47 +0530

    Custom NAT support in Azure virtual network

--- vnet.tf
+++ vnet.tf
@@ -20,6 +20,37 @@ module "vnet" {
   }
 }
 
+// Custom NAT gateway
+resource "azurerm_public_ip" "nat_ip" {
+  count               = !var.use_default_nat && var.use_external_public_addresses ? 1 : 0
+  name                = "${var.cluster_name}-nat-ip"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Static"
+  tags                = var.tags
+  sku                 = var.public_ip_address_sku
+}
+
+resource "azurerm_nat_gateway" "nat_gateway" {
+  count               = var.use_default_nat ? 0 : 1
+  name                = "${var.cluster_name}-nat-gw"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "nat_gateway_public_ip_association" {
+  count                = var.use_default_nat ? 0 : 1
+  nat_gateway_id       = azurerm_nat_gateway.nat_gateway[0].id
+  public_ip_address_id = var.use_external_public_addresses ? var.external_public_address_id : azurerm_public_ip.nat_ip[0].id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "nat_gateway_association" {
+  count          = var.use_default_nat ? 0 : 1
+  subnet_id      = var.use_existing_vnet ? var.subnet_id : module.vnet[0].vnet_subnets[0]
+  nat_gateway_id = azurerm_nat_gateway.nat_gateway[0].id
+}
+
 // creating a private dns zone
 resource "azurerm_private_dns_zone" "postgres_dns" {
   count               = var.control_plane_enabled ? 1 : 0
